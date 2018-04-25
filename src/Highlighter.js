bfb21510efa4013a9a55e02f283b1c6ee17019c2
Steven Chester <steven.chester@industryweapon.com>
2018-04-25 11:50:08 -0400

    fix isValid in highlighter

--- src/Highlighter.js
+++ src/Highlighter.js
@@ -1,21 +1,22 @@
 import * as React from "react"
 
 const Highlighter = props => {
-    // TODO: Currently this component renders only one child. Should support the entire {props.children}
-    const child = props.children[0]
-
-    // secondaryRegex allows for a more granular check on the regex
-    const hasRegex = !!props.secondaryRegex
-
-    // can match the text against an object of strings
-    const variableExists = !!props.variables[child.props.text]
+	// TODO: Currently this component renders only one child. Should support the entire {props.children}
+	const child = props.children[0]
 	const theme = props.theme || {}
 
-	// if the user supplied variables, check to see if the variable name exists.
-	// if they did not supply variables, check to see if they supplied a secondary regex to match against
-    // else just default to true
-    const isValid = variableExists ? true : hasRegex ? props.secondaryRegex.exec(child.props.text) : true
+	const variableExists = !!props.variables[child.props.text]
+	const hasRegex = !!props.secondaryRegex
+	const hasVariables = Object.keys(props.variables).length > 0
 
+	let isValid = true
+
+	if (hasVariables) {
+		isValid = variableExists
+	} else if (hasRegex) {
+		isValid = !!props.secondaryRegex.exec(child.props.text)
+    }
+    
 	return <span style={isValid ? theme.valid : theme.invalid}>{child}</span>
 }
 
